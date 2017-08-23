85fe4cb8cfa0a74bda4910c7e37963cb5b60640a
Lcfvs <michael.rouges@gmail.com>
2017-08-23 19:35:33 +0200

    + demo files

---
+++ demos/form-validation/script.js
@@ -0,0 +1,18 @@
+void function (global) {
+  'use strict';
+
+  let
+  anticore = global.anticore,
+  $ = anticore.utils.$;
+
+  anticore
+  .on('.error[data-for]', function(element, next) {
+    let target = element.dataset.for;
+    let field = $(target);
+    field.parentNode.insertBefore(element, field);
+
+    next();
+  })
+  .defaults()
+  .populate();
+}(this);
---
+++ demos/readme.md
@@ -0,0 +1,9 @@
+# Anticore demos
+
+In console, go to your localhost directory then run this :
+
+```sh
+git clone https://github.com/Lcfvs/anticore.git anticore
+```
+
+Then open your browser at [http://localhost/anticore/demos/index.html](http://localhost/anticore/demos/index.html)
