2a22ada6baa248475f6d58b0f3483bdeaecb87c7
Lcfvs <michael.rouges@gmail.com>
2018-02-07 21:46:40 +0100

    closestOrSelf fix when node is not an element

--- dom/query/closestOrSelf/index.js
+++ dom/query/closestOrSelf/index.js
@@ -1,8 +1,9 @@
+import {isElement} from '../../info/isElement';
 import {matches} from '../../info/matches';
 import {closest} from '../closest';
 
 export function closestOrSelf(selector, node) {
-  return matches(selector, node)
-    ? node
-    : closest(selector, node);
+  return isElement(node) && matches(selector, node)
+  ? node
+  : closest(selector, node);
 }
