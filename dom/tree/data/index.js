a542827d5900c5c221baddf499c9f5b0843f33ee
Lcfvs <michael.rouges@gmail.com>
2018-02-08 14:42:07 +0100

    fix for data(element, value, null)

--- dom/tree/data/index.js
+++ dom/tree/data/index.js
@@ -6,8 +6,14 @@ import {dashToCamel} from '../../../primitive/string/dashToCamel';
 export function data(element, name, value) {
   name = dashToCamel(name);
 
-  if (isString(value) || isNumber(value) || isNull(value)) {
+  if (isString(value) || isNumber(value)) {
     element.dataset[name] = value;
+
+    return element;
+  }
+
+  if (isNull(value)) {
+    delete element.dataset[name];
 
     return element;
   }
