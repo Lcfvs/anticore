4ec4a168ec028e0ec0dbe557e72593711bb7eb96
Lcfvs <michael.rouges@gmail.com>
2018-03-29 18:09:54 +0200

    True data: prefixed dataURLs

--- api/blob/blobToDataURL/index.js
+++ api/blob/blobToDataURL/index.js
@@ -1,14 +1,23 @@
 import {global} from '../../../global';
+import {curry} from '../../../primitive/function/curry';
 import {promise} from '../../../primitive/function/promise';
 
 const
 window = global(),
-createObjectURL = window.URL.createObjectURL;
+FileReader = window.FileReader;
 
 export function blobToDataURL(blob) {
   return promise(read, blob);
 }
 
 function read(blob, resolve) {
-  resolve(createObjectURL(blob));
+  const
+  reader = new FileReader();
+
+  reader.addEventListener('load', curry(onLoad, resolve));
+  reader.readAsDataURL(blob);
+}
+
+function onLoad(resolve, event) {
+  resolve(event.target.result);
 }
