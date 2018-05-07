adf247bcb1558672cce3e93c44b06326cff4fd87
Lcfvs <michael.rouges@gmail.com>
2018-05-07 19:53:38 +0200

    + dom\tree\sanitize & sanitizing html()

--- dom/tree/html/index.js
+++ dom/tree/html/index.js
@@ -1,12 +1,14 @@
+import { forEach } from '../../../primitive/array/forEach'
 import { isUndefined } from '../../../primitive/undefined/isUndefined'
 import { isElement } from '../../info/isElement'
+import { elements } from '../../query/elements'
 import { sanitize } from '../sanitize'
 
 export function html (element, data) {
   if (isElement(element)) {
     if (!isUndefined(data)) {
       element.innerHTML = data
-      sanitize(element.innerHTML)
+      forEach(elements(element), sanitize)
 
       return element
     }
