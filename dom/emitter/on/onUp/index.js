9a02eafc8ae72da9b33a4ac0f71dd0c05270e754
Lcfvs <michael.rouges@gmail.com>
2018-08-26 15:24:30 +0200

    typo fix

--- dom/emitter/on/onUp/index.js
+++ dom/emitter/on/onUp/index.js
@@ -3,7 +3,7 @@ import { matches } from '../../.matches'
 
 export function onUp (element, listener, useCapture) {
   return on('keydown', element, function (event) {
-    if (matches(event, false, false, fase, false, ['ArrowUp', 'Up'], 39)) {
+    if (matches(event, false, false, false, false, ['ArrowUp', 'Up'], 39)) {
       return listener.call(this, event)
     }
   }, useCapture)
