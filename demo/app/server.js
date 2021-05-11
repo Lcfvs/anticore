f5597c3033d7a57b0a7a894de436e6293cbb172a
Brett Zamir <brettz9@yahoo.com>
2021-05-11 08:41:24 +0800

    - Add open option

--- demo/app/server.js
+++ demo/app/server.js
@@ -1,8 +1,13 @@
+import open from 'open';
 import app from './app.js'
 
 import './routes/home.js'
 import './routes/form.js'
 
-const listener = app.listen(process.env.PORT, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
+const listener = app.listen(process.env.PORT, async () => {
+  const {port} = listener.address();
+  console.log(`Your app is listening on port ${port}`)
+  if (process.argv.includes('--open')) {
+    await open(`http://127.0.0.1:${port}`);
+  }
 })
